c35dd99260c600a20d1c24c62937776e4ad8c523
chekulhan <46518731+chekulhan@users.noreply.github.com>
2024-01-22 13:12:38 +0000

    initial app

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import streamlit as st
 from openai import OpenAI
 
+from time import sleep
 """
 # Welcome to Streamlit!
 
@@ -13,6 +14,52 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
+
+
+client = OpenAI(api_key=st.secrets["API_KEY"])
+ASSISTANT_ID = st.secrets["ASSISTANT_ID"]
+
+assistant = client.beta.assistants.create(
+    name="KW Educación Asistente",
+    instructions="",
+    tools=[{"type": "code_interpreter"}],
+
+)
+
+thread = client.beta.threads.create()
+
+message = client.beta.threads.messages.create(
+    thread_id=thread.id,
+    role="user",
+    content="¿Me puedes ayudar con información sobre la ley de educación?",
+    
+)
+
+
+run = client.beta.threads.runs.create(
+  thread_id=thread.id,
+  assistant_id=ASSISTANT_ID
+)
+
+st.write(run)
+
+while (run.status != "completed") or (run.failed_at == "None"):
+    st.write(f"Working... {run.status}")
+    st.write(run)
+    run = client.beta.threads.runs.retrieve(
+        thread_id=thread.id,
+        run_id=run.id
+    )
+    sleep(8)
+
+st.write(run)
+
+messages = client.beta.threads.messages.list(
+  thread_id=thread.id
+)
+
+
+st.write(messages)
 
 num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
 num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
